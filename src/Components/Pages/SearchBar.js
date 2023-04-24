1f9744089a70b3cbb48adc17743ebe1b31d428a7
Tethet <tethet@Entethedayush.kumar.cse22@itbhu.ac.in>
2023-04-24 16:25:53 +0530

    made fonts responsive

---
+++ src/Components/Pages/SearchBar.js
@@ -0,0 +1,45 @@
+import SearchIcon from '@mui/icons-material/Search';
+import React,{ useState } from 'react';
+import { Link } from 'react-router-dom';
+import ClearIcon from '@mui/icons-material/Clear';
+const SearchBar = ({data}) => {
+    const [FilteredData, SetFilteredData]=useState([]);
+    const [wordEntered,setWordEntered]=useState("");
+    const handleFilter=(event)=>{
+        const searchWord=event.target.value;
+        setWordEntered(searchWord);
+        const newFilter=data.filter((value)=>{
+            return value.Description.toLowerCase().includes(searchWord.toLowerCase())
+        });
+        if(searchWord ===""){
+            SetFilteredData([]);
+        }
+        else
+        SetFilteredData(newFilter);
+    }
+    const ClearInput=()=>{
+        SetFilteredData([]);
+        setWordEntered("");
+    }
+    return ( 
+        <>
+            <div className="rounded-[10px] h-[6vh] w-[40vw] bg-[#E0E2E1] flex items-center gap-[10px] pl-[6px] max-w-[700px]">
+                <SearchIcon/> 
+                <input type="text" placeholder="Search" value={wordEntered} className="focus:outline-none mr-[2px] rounded-[10px] text-[2rem] font-[400] text-[#666666] bg-[#E0E2E1] w-[30vw]" onChange={handleFilter}/>
+            </div>
+            {/* {FilteredData.length!==0 && (
+            <div className="searchResult m-3 mt-1 w-[300px] h-[400px] overflow-y-auto p-2 leading-10">
+            {FilteredData.slice(0,15).map((value,key)=>{
+                return(
+                <Link to="/" >
+                    <p className='hover:bg-[grey]'>{value.Description}</p>
+                </Link>
+                )
+            })}
+            </div>
+            )} */}
+        </>
+     );
+}
+ 
+export default SearchBar;
---
+++ src/Components/Shop/Tiles.js
@@ -0,0 +1,32 @@
+const Tiles=({Photo,Name,Description,Pricing,Discount,Review,ReviewCount})=>{
+    let cost=Pricing*(100-Discount)/100;
+    return(
+        <>
+            <div className="flex flex-col text-center w-[200px] m-2 font-Ubuntu border-2 rounded-[10px] drop-shadow-2xl pb-[20px] shadow-lg">
+            <img src={require("../../ProductImages/Product1.jpg")} alt="Product" width={100} height={100} className="self-center"/>
+                <div className="text-lg font-bold">
+                    {Name}
+                </div>
+                <div className='text-base'>
+                    {Description}
+                </div>
+                <div>
+                    <div>
+                    <del className="font-light">&#8377;{Pricing}</del> 	&#8377;{cost}
+                    <p className="text-orange-700">
+                        {Discount}% off
+                    </p>
+                    </div>
+                </div>
+                <div className="Review">
+                    <span className="rev_in fas fa-star"></span>
+                    <span className="rev_in fas fa-star"></span>
+                    <span className="rev_in fas fa-star"></span>
+                    <span className="rev_in fas fa-star"></span>
+                    <span className="rev_in fas fa-star"></span>
+                </div>
+            </div>
+        </>
+    )
+}
+export {Tiles};
